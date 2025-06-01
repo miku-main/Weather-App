7225838c088088cbc3361f8ab14dc8af5da208da
miku-main <miku-main@users.noreply.github.com>
2025-06-01 05:14:59 +0000

    deploy: 2da0cea3da2a26dc4f50abdd8ab3085f0b2192a7

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js (this will be replaced by GitHub Actions)
-var API_KEY = "735997140a17a88e2ba650caeb457021"; // Placeholder
+var WEATHER_API_KEY = "ce5d86554f8622f5b4a9275c3589c0f7"; // Placeholder
