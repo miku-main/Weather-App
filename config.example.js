48dd3cb62c9874a76eb6046ce727a4c6beb1c1d7
miku-main <miku-main@users.noreply.github.com>
2025-06-01 05:00:03 +0000

    deploy: f667ac833c84aa10460d746b656f88e68e19c574

---
+++ config.example.js
@@ -0,0 +1,2 @@
+// config.example.js (for refeerence)
+const API_KEY = "paste_you_key_here";
---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js (this will be replaced by GitHub Actions)
+const API_KEY = "735997140a17a88e2ba650caeb457021"; // Placeholder
