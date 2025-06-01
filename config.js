2da0cea3da2a26dc4f50abdd8ab3085f0b2192a7
Gui Sen Zou <125292379+miku-main@users.noreply.github.com>
2025-06-01 01:14:44 -0400

    Changing api key name

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js (this will be replaced by GitHub Actions)
-var API_KEY = "__API_KEY__"; // Placeholder
+var WEATHER_API_KEY = "__API_KEY__"; // Placeholder
--- script.js
+++ script.js
@@ -32,7 +32,7 @@ cityInput.addEventListener('keydown', (event) => {
 
 
 async function getFetchData(endPoint, city) {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${API_KEY}&units=metric`
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${WEATHER_API_KEY}&units=metric`
 
     const response = await fetch(apiUrl)
 
